925e7c7cb0246b9d5dc74a31565db89aab708683
syedm1 <31075630+syedm1@users.noreply.github.com>
2021-06-15 02:43:48 +1000

    clean up for swagger docs

--- server.js
+++ server.js
@@ -11,46 +11,17 @@ const reviewsRouter = require("./server/routes/reviews");
 const publicPath = path.join(__dirname, ".", "public");
 require("./server/config/mongoConnection.js");
 require("./server/middlewares/cors")(app);
+const SwaggerOptions = require("./swagger");
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(logger("dev"));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-const swaggerDefinition = {
-  openapi: "3.0.0",
-  info: {
-    title: "Express API for JSONPlaceholder",
-    version: "1.0.0",
-    description:
-      "This is a REST API application made with Express. It retrieves data from JSONPlaceholder.",
-    license: {
-      name: "Licensed Under MIT",
-      url: "https://spdx.org/licenses/MIT.html",
-    },
-    contact: {
-      name: "JSONPlaceholder",
-      url: "https://jsonplaceholder.typicode.com",
-    },
-  },
-  servers: [
-    {
-      url: "http://localhost:3000/api/reviews",
-      description: "Development server",
-    },
-  ],
-};
-
-const options = {
-  swaggerDefinition,
-  // Path to the API docs
-  apis: ["./server/routes/reviews.js"],
-};
-
 app.use(express.static(publicPath));
 
 app.use("/api", reviewsRouter);
-const specs = swaggerJSDoc(options);
+const specs = swaggerJSDoc(SwaggerOptions);
 app.use(
   "/swagger",
   swaggerUi.serve,
@@ -63,6 +34,7 @@ app.get("/*", (req, res) => {
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
+  console.log("404");
   next(createError(404));
 });
 
---
+++ swagger.js
@@ -0,0 +1,35 @@
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Bhalu API documentation",
+    version: "1.0.0",
+    description:
+      "This is a REST API application made with Express for Bhalu APP.",
+    license: {
+      name: "Licensed Under MIT",
+      url: "https://spdx.org/licenses/MIT.html",
+    },
+    contact: {
+      name: "Sharukh",
+      email: "sdsharukh9@gmail.com",
+    },
+  },
+  servers: [
+    {
+      url: "http://localhost:3000/api/",
+      description: "Development server",
+    },
+    {
+      url: "http://bhalu.io/api/",
+      description: "Production server",
+    },
+  ],
+};
+
+const SwaggerOptions = {
+  swaggerDefinition,
+  // Path to the API docs
+  apis: ["./server/routes/*.js"],
+};
+
+module.exports = SwaggerOptions;
